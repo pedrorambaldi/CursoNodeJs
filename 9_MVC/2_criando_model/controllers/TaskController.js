30ff712b80f269a6c2bdf3b65a2b4cb579551dda
Pedro Rambaldi <rambaldipedro@gmail.com>
2022-11-01 15:53:44 -0300

    criando rotas

--- 9_MVC/2_criando_model/controllers/TaskController.js
+++ 9_MVC/2_criando_model/controllers/TaskController.js
@@ -4,4 +4,8 @@ module.exports = class TaskController {
   static createTask(req, res) {
     res.render("tasks/create");
   }
+
+  static showTask(req, res) {
+    res.render("tasks/all");
+  }
 };
--- 9_MVC/2_criando_model/index.js
+++ 9_MVC/2_criando_model/index.js
@@ -7,6 +7,8 @@ const conn = require("./db/conn");
 
 const Task = require("./models/Task");
 
+const taskRoutes = require('./routes/taskRoutes')
+
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
@@ -19,6 +21,8 @@ app.use(
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.use('/tasks', taskRoutes)
 
 conn
   .sync()
