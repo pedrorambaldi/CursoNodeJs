b6916c883b9bc0096c08e87d396a8f28dce06131
Pedro Rambaldi <rambaldipedro@gmail.com>
2022-11-01 18:14:46 -0300

    atualizando dados

--- 9_MVC/2_criando_model/controllers/TaskController.js
+++ 9_MVC/2_criando_model/controllers/TaskController.js
@@ -25,6 +25,27 @@ module.exports = class TaskController {
     res.redirect("/tasks");
   }
 
+  static async updateTask(req, res) {
+    const id = req.params.id;
+
+    const task = await Task.findOne({ where: { id: id }, raw: true });
+
+    res.render("tasks/edit", { task });
+  }
+
+  static async updateTaskPost(req, res) {
+    const id = req.body.id;
+
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+    };
+
+    await Task.update(task, { where: { id: id } });
+
+    res.redirect("/tasks");
+  }
+
   static async showTask(req, res) {
     const tasks = await Task.findAll({ raw: true });
 
--- 9_MVC/2_criando_model/routes/taskRoutes.js
+++ 9_MVC/2_criando_model/routes/taskRoutes.js
@@ -6,6 +6,8 @@ const TaskController = require('../controllers/TaskController')
 router.get('/add', TaskController.createTask)
 router.post('/add', TaskController.createTaskSave)
 router.post('/remove', TaskController.removeTask)
+router.get('/edit/:id', TaskController.updateTask)
+router.post('/edit', TaskController.updateTaskPost)
 router.get('/', TaskController.showTask)
 
 module.exports = router
