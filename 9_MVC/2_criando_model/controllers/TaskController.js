9ec2496dde5bf0716508515a24826bd18b6fbe12
Pedro Rambaldi <rambaldipedro@gmail.com>
2022-11-01 17:41:23 -0300

    removendo dados

--- 9_MVC/2_criando_model/controllers/TaskController.js
+++ 9_MVC/2_criando_model/controllers/TaskController.js
@@ -17,9 +17,17 @@ module.exports = class TaskController {
     res.redirect("/tasks");
   }
 
+  static async removeTask(req, res) {
+    const id = req.body.id;
+
+    await Task.destroy({ where: { id: id } });
+
+    res.redirect("/tasks");
+  }
+
   static async showTask(req, res) {
     const tasks = await Task.findAll({ raw: true });
 
-    res.render("tasks/all", {tasks});
+    res.render("tasks/all", { tasks });
   }
 };
